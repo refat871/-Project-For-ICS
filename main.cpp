d8d0af8c90f36ef84932d148ecd16fc1555315e3
refat871 <57785695+refat871@users.noreply.github.com>
2019-12-04 10:24:48 +0600

    ODD EVEN NUMBER
    ODD & EVEN

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter an imteger number:";
+    cin>>a;
+    if(a%2==0)
+        cout<<"Even number"<<endl;
+    else
+        cout<<"Odd number"<<endl;
+    return 0;
+}
